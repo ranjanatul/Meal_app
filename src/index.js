2947a93f44c95d27ba3a64ee55a1749d77594ff9
Atul Ranjan <atul.ranjan100@gmail.com>
2022-10-12 15:16:22 +0530

    Update index.js
    adding js changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,49 @@
-import "./styles.css";
+const searchByMealName = document.getElementById('mealName');
+const mealSelected = document.getElementById('mealSelected');
+let result = [];
+let filteredResult = [];
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+searchByMealName.addEventListener('keyup', function(event) {
+  const key = event.target.value;
+  if (key.length === 0) {
+    mealSelected.innerHTML = '<option>Type to get suggestions</option>'; 
+  }
+  else if (key.length === 1) {
+    result = [];
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${key}`)
+    .then((response) => {
+      return response.json()
+      })
+      .then(response => {
+        const {meals = []} = response;
+      if (meals) {
+        result.push(...meals);
+        result.forEach(({idMeal, strMeal}) => {
+          const option = document.createElement('option');
+          option.setAttribute('value', idMeal);
+          option.innerHTML = strMeal;
+          mealSelected.append(option);
+        })
+      } else {
+        mealSelected.innerHTML = '<option>No data Found</option>';
+      }
+    })
+  } else if (key.length > 1) {
+    filteredResult = result.filter(({strMeal}) => {
+      return (strMeal.toLowerCase().indexOf(key) > -1)
+    })
+    mealSelected.innerHTML = '';
+    if (filteredResult.length == 0) {
+     mealSelected.innerHTML = '<option>No data Found</option>'; 
+    } else {
+      mealSelected.innerHTML = '';
+      filteredResult.forEach(({idMeal, strMeal}) => {
+        const option = document.createElement('option');
+        option.setAttribute('value', idMeal);
+        option.innerHTML = strMeal;
+        mealSelected.append(option);
+      })
+    }
+    
+  }
+})
